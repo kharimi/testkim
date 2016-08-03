294a929a00a764a638f07d5b9364cc7ac220b676
kharimigit config --global user.email a.kim@a4-agency.rugit config --global user.name kharimi <a.kim@a4-agency.ru>
2016-08-03 19:43:51 +0300

    Init

---
+++ Form.php
@@ -0,0 +1,8 @@
+﻿<?php 
+$name=$_POST['name']; 
+$birthday=strtotime($_POST['birthday']); 
+$today=time(); 
+$diff=$today-$birthday; 
+$result=round($diff /60/60/24) . "days"; 
+echo $name. ", бла".$result." дней"; 
+?>
